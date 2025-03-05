726579470a4f057c3ca9e6316111e565c1b2a5c3
Murali8327 <muralireddypatil@gmail.com>
2025-03-05 16:19:46 +0530

    Test new GitHub account

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, This is My First Website."
+    return "Hello, This is Murali"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
