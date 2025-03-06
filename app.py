86cee41cf5cb6bbf642825f9fd40a1692daec7ac
Murali8327 <muralireddypatil@gmail.com>
2025-03-06 16:39:51 +0530

    Test New Github Account

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, This is Murali,koushik from Docker"
+    return "Hello, This is Murali from Docker"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
